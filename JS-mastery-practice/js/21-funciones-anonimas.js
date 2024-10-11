a5d11bc4076c4df4978e4fdd1db2dfed1cd93ccc
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-10-11 11:46:00 +0200

    Intro a las funciones con flecha

--- JS-mastery-practice/js/21-funciones-anonimas.js
+++ JS-mastery-practice/js/21-funciones-anonimas.js
@@ -1,8 +1,44 @@
-'use strict'
+"use strict"; // Activa el modo estricto
 
 // Una función anónima es una función que no tiene nombre 
 
-
-var  pelicula = function(nombre){
-    return "La pelicula es: " + nombre
+var pelicula = function(nombre) {
+    return "La película es: " + nombre;
 }
+
+// Un callback es una función dentro de otra función
+// Se hará una función de callback con sumaYmuestra y sumaPorDos
+
+function sumame(num1, num2, sumaYmuestra, sumaPorDos) {
+    var sumar = num1 + num2; // Calculamos la suma
+    sumaYmuestra(sumar); // Llamamos al primer callback
+    sumaPorDos(sumar); // Llamamos al segundo callback
+
+    return sumar; // Devolvemos el resultado de la suma
+}
+
+//En las funciones con flecha con poner el parámetro es suficiente 
+
+sumame(5,5, dato =>{
+    console.log("La suma es:", dato);
+},
+(dato) => {
+    console.log("La suma multiplicado por dos es :", (dato* 2));
+}
+
+
+
+);
+
+
+// Para que se vea en consola siempre debe usarse console.log (manera simple)
+console.log(sumame(2, 3, 
+    function(dato) {
+        console.log("La suma es:", dato);
+    }, 
+    function(dato) {
+        console.log("La suma multiplicada por dos es:", (dato * 2));
+    })
+);
+
+// Imprimiendo en consola con callback
