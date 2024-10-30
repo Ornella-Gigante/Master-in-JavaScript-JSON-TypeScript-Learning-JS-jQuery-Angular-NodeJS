6b57baa1358961a8871c7653ba319ad057a7973e
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-10-30 19:39:20 +0100

    practicando try-catch con file nuevo

---
+++ JS-mastery-practice/js/37-try.catch.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Probando que el script funcioan bien en el html 
+
+alert("Holis")
+
+
+// Controlar error con la estrutura try-catch 
+// Todo codigo susceptible a fallos debe ir al try 
+
+
+try{
+    var year = 2024;
+    alert(yea); // error hecho a posta para probar el try-catch 
+}catch(error){
+    console.log(error); 
+    alert("Ha ocurrido un error en el código");
+
+}; 
