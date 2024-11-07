272a0f8e9b9839d9865cb010ee28b77b9100f651
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-11-07 20:00:05 +0100

    Metodo LOAD() in Ajax, practice

---
+++ JS-mastery-practice/js/43-Ajax.js
@@ -0,0 +1,9 @@
+'use strict'
+
+$(document).ready(function(){
+    
+    // FUNCION LOAD
+    // Recoge info de una web para meterlo dentro de un div a través de un GET 
+    
+    $("#datos").load("https://reqres.in/");
+});
