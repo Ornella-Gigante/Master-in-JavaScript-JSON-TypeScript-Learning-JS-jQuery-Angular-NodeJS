5fb8fb4a64f1e7d4dcf4bbd25d4cd45ce2f94bc3
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-11-07 21:17:24 +0100

    Metodo GET practica

--- JS-mastery-practice/js/43-Ajax.js
+++ JS-mastery-practice/js/43-Ajax.js
@@ -1,9 +1,29 @@
 'use strict'
 
 $(document).ready(function(){
-    
     // FUNCION LOAD
-    // Recoge info de una web para meterlo dentro de un div a través de un GET 
+    // Recoge la lista de usuarios de reqres.in
+    // $("#datos").load("https://reqres.in/api/users");
     
-    $("#datos").load("https://reqres.in/");
+    // METODO GET Y POST
+    // Obtiene un usuario específico (en este caso el usuario con ID 1)
+    $.get("https://reqres.in/api/users/1", function(response){
+
+              // Accede directamente a la propiedad 'data' de la respuesta
+            const userData = response.data;
+            $("#datos").append("<p>"+userData.first_name + "" + "<p>" + userData.last_name);
+
+            
+        
+    });
+
+    // Uso de parametros de consulta
+    // page = numero de pagina 
+    // per_page = cantidad de resultados que se quieren sacar por página (3 users) 
+    $.get("https://reqres.in/api/users", {
+        page: 3,
+        per_page: 3
+    }, function(response){
+        console.log(response);
+    });
 });
