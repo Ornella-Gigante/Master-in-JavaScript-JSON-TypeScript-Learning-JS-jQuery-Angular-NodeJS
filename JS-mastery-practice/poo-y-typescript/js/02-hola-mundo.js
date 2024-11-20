fe3f4e0aefc7bef73055b4531066b82948d92358
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-11-20 17:33:20 +0100

    Creaciond e primer file .ts y documentación sobre tipos de compilación con TS

---
+++ JS-mastery-practice/poo-y-typescript/js/02-hola-mundo.js
@@ -0,0 +1,4 @@
+// Creacion de codigo TypeScript que al compilar sera JavaScript 
+// Se hara una traspilacion , usando el cpmando tsc para pasar de TS a JS
+console.log("Hola mundo con TypeScript");
+alert("Esto es una alerta");
---
+++ JS-mastery-practice/poo-y-typescript/js/02-hola-mundo.ts
@@ -0,0 +1,6 @@
+// Creacion de codigo TypeScript que al compilar sera JavaScript 
+// Se hara una traspilacion , usando el cpmando tsc para pasar de TS a JS
+// Siempre que se hace un cambio en .ts se debe hacer la traspilacion con tsc para que pase a JS y se vea el cambio en el HTML 
+
+console.log("Hola mundo con TypeScript");
+alert("Esto es una alerta");
