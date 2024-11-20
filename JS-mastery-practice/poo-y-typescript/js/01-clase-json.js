7c1e8995d81c164e86b63b764b1e6b8064b75863
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-11-20 08:56:40 +0100

    Creacion de clase y Metodos en JS , practica de POO

--- JS-mastery-practice/poo-y-typescript/js/01-clase-json.js
+++ JS-mastery-practice/poo-y-typescript/js/01-clase-json.js
@@ -1,6 +1,27 @@
 
-// Creación del o
+// Creación del objeto bicicleta 
 var bicicleta = {
 
+    // Creación de atributos 
+
+    color: 'Rojo',
+    modelo: 'BMX',
+    frenos: 'De disco',
+    velocidadMaxima:'60km/h',
+
+
+
+    //Metodo de cambio de color 
+
+    cambiaColor: function(nuevo_color){
+        this.color = nuevo_color; 
+        console.log(this);
+    }
+
 };
+
+//Cambio de color llamando al método 
+// En consola el color de la bicicleta se verá a azul 
+
+bicicleta.cambiaColor("Azul");
 
