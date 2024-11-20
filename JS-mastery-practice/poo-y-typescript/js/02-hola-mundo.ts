8f385577912e099fdcbe2904eff14d83607bb3fd
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-11-20 17:47:59 +0100

    comandos de compilacion automaticos npm install -g live-server y npm install -g live-server y documentacion

--- JS-mastery-practice/poo-y-typescript/js/02-hola-mundo.ts
+++ JS-mastery-practice/poo-y-typescript/js/02-hola-mundo.ts
@@ -3,4 +3,4 @@
 // Siempre que se hace un cambio en .ts se debe hacer la traspilacion con tsc para que pase a JS y se vea el cambio en el HTML 
 
 console.log("Hola mundo con TypeScript");
-alert("Esto es una alerta");
+alert("Esto es otra alerta");
