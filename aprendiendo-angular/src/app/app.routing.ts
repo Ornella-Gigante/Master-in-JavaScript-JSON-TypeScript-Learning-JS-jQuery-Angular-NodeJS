38ff6adb16fad19be1762efeacaa5c0bbd8315e3
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-11-27 16:23:31 +0100

    CReacion de file routing.ts con imports de componentes

---
+++ aprendiendo-angular/src/app/app.routing.ts
@@ -0,0 +1,12 @@
+/**
+ * EN ESTE ARCHIVO IRÁ TODA LA CONFIGURACIŃO DE RUTAS 
+ * DE LA APLICACIÓN DE ANGULAR 
+ * SE CONFIGURARÁ EL ROUTER Y SE DARÁ DE ALTA LA ROUTA DE LA APP 
+ * 
+ */
+
+import { ModuleWithProviders } from "@angular/core";
+import {Routes, RouterModule} from '@angular/router';
+import { ZapatillasComponent} from "./zapatillas/zapatillas.component";
+import { VideojuegoComponent } from "./videojuego/videojuego.component";
+
