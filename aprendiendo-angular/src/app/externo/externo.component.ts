532eca7fbc7977a3eabf0158f9c5864c5d286d09
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2024-12-20 20:13:37 +0100

    creacion componente externo

---
+++ aprendiendo-angular/src/app/externo/externo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-externo',
+  imports: [],
+  templateUrl: './externo.component.html',
+  styleUrl: './externo.component.css'
+})
+export class ExternoComponent {
+
+}
