6bf443933e18495a86aa91a0338666dd10329287
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-01-20 17:42:43 +0100

    Corrección de estilo en componente externo

--- aprendiendo-angular/src/app/pipes/calculadora.pipe.ts
+++ aprendiendo-angular/src/app/pipes/calculadora.pipe.ts
@@ -13,9 +13,9 @@ export class CalculadoraPipe implements PipeTransform{
     transform(value: any, value_two: any){
         let operaciones = `
 
-        Suma: ${value + value_two} <br/>
-        Resta: ${value - value_two}<br/> 
-        Multiplicacion: ${value*value_two}<br/>
+        Suma: ${value + value_two} 
+        Resta: ${value - value_two}
+        Multiplicacion: ${value*value_two}
         Division: ${value/value_two}
         `
         ; 
