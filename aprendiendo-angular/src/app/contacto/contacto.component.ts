9612064cf254e5f24743f33f124bd7b83a64c2a1
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-01-20 18:11:27 +0100

    Creacion de formulario componente contacto

--- aprendiendo-angular/src/app/contacto/contacto.component.ts
+++ aprendiendo-angular/src/app/contacto/contacto.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactoUsuario } from '../model/contacto.usuario';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit{
+  public usuario: ContactoUsuario;
+
+  constructor() {
+    this.usuario = new ContactoUsuario('', '', '', '');
+  }
+  
+
+  ngOnInit(): void {
+      
+  }
 
 }
