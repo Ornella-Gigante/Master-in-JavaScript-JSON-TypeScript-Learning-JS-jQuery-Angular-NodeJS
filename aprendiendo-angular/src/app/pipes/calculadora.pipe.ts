c05583e7a996224208bff8467d5309a006476980
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-01-20 12:40:54 +0100

    creacion clase calculadoraPipe

---
+++ aprendiendo-angular/src/app/pipes/calculadora.pipe.ts
@@ -0,0 +1,26 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+
+    name: 'calculadora'
+
+
+})
+
+export class CalculadoraPipe implements PipeTransform{
+
+    //tendra dos parametros 
+    transform(value: any, value_two: any){
+        let operaciones = `
+
+        Suma: ${value + value_two} <br/>
+        Resta: ${value - value_two}<br/> 
+        Multiplicacion: ${value*value_two}<br/>
+        Division: ${value/value_two}
+        `
+        ; 
+
+        return operaciones; 
+
+    }
+}
